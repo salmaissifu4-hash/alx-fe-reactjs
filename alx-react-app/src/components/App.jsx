616dba91c1df77b60fa7f0fc11112de29122e81b
Salma <salmaissifu4@gmail.com>
2025-11-02 21:31:55 +0000

    Created Header, MainContent, and Footer components and integrated into App.jsx

--- alx-react-app/src/components/App.jsx
+++ alx-react-app/src/components/App.jsx
@@ -1,10 +1,14 @@
-import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <>
-      <WelcomeMessage />
-    </>
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
   );
 }
 
