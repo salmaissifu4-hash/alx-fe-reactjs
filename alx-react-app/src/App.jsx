394e420d44b0d6a376b1af0aae5ade6660c3a2be
Salma <salmaissifu4@gmail.com>
2025-11-02 22:09:23 +0000

    Added WelcomeMessage component and imported it into App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,11 +2,13 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import WelcomeMessage from './components/WelcomeMessage';
 
 function App() {
   return (
     <div>
       <Header />
+      <WelcomeMessage />
       <MainContent />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -1,12 +1,9 @@
-
-function WelcomeMessage() {
-    return (
-        <div>
-            <h1>Hello everyone, I am learning React at ALX!</h1>
-            <p>This is a simple JSX component.</p>
-            <p>I am learning about JSX!</p>
-        </div>
-    );
-}
+const WelcomeMessage = () => {
+  return (
+    <div>
+      <p>Hello everyone, I am learning React at ALX!</p>
+    </div>
+  );
+};
 
 export default WelcomeMessage;
