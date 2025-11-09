28dda00e8bf0c067215a7a8436084973b621c00b
Salma Godiya Issifu <salmaissifu4@gmail.com>
2025-11-09 20:14:58 +0000

    Refactor App component to include counter and welcome message

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,17 +1,46 @@
-import Header from './components/Header';
-import UserProfile from './components/UserProfile';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import WelcomeMessage from './WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+  import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
+import Counter from './Counter.jsx';
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div>
+    <>
+      <div>
+      <Counter></Counter>
       <Header />
-      <UserProfile name="Salma Issifu" age="23" bio="Frontend Developer passionate about React." />
       <MainContent />
       <Footer />
-    </div>
-  );
+      <WelcomeMessage />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />       
+       <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
